ed95bafbad313012f322087edd774980db8c7da8
Jim Peters <jimjitsuesq@gmail.com>
2021-05-23 15:09:03 -0700

    Final draft version for submission

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-photo-gallery
+# student-directory
  
--- js/script.js
+++ js/script.js
@@ -12,51 +12,41 @@ For assistance:
 */
 
 const ul = document.querySelector('.student-list');
-const li = '';
 const studentArray = [];
 let splitArray = [];
 let numberOfPages = 0;
 let currentPage = 1;
 
-function CreateLI (arr) {
+/* 
+Takes the array of student objects in `data`, formats it and inserts it into a new array called `studentArray` using the function `createLI`
+*/
+
+function createLI (arr) {
    for (let i = 0; i < arr.length; i++) {
-   const li = document.createElement('li');
-   const SDdiv = document.createElement('div');
-   const JDdiv = document.createElement('div');
-   const pic = document.createElement('img');
-   const name = document.createElement('h3');
-   const email = document.createElement('span');
-   const joinedDate = document.createElement('span');
-   li.className = "student-item"
-   SDdiv.className = "student-details";
-   li.appendChild(SDdiv);
-   pic.className = "avatar";
-   pic.src = `${arr[i].picture.large}`;
-   pic.alt = 'Profile Picture';
-   SDdiv.appendChild(pic);
-   name.innerHTML = `${arr[i].name.first} ${arr[i].name.last}`;
-   SDdiv.appendChild(name);
-   email.className = "email";
-   email.innerHTML = `${arr[i].email}`;
-   SDdiv.appendChild(email);
-   JDdiv.className = "joined-details";
-   li.appendChild(JDdiv);
-   joinedDate.innerHTML = `Joined ${arr[i].registered.date}`;
-   JDdiv.appendChild(joinedDate); 
+   li = document.createElement('li');
+   li.className = "student-item";
+   li.innerHTML = `<div class="student-details">
+      <img class="avatar" src="${arr[i].picture.large}"; alt="Profile Picture">
+      <h3>${arr[i].name.first} ${arr[i].name.last}</h3>
+      <span class="email">${arr[i].email}</span>
+      </div>
+      <div class="joined-details">
+      <span class="date">Joined ${arr[i].registered.date}</span>
+      </div>`;
    studentArray.push(li);
    }
 }
 
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Creates the `sliceArray` function, which splits the studentArray into 9-record pages 
+Creates the `showPage` function, which creates the page of student records
 */
 
 function sliceArray (arr) {
    splitArray = [];
    const slice = arr.slice((currentPage - 1) * 9, ((currentPage-1) * 9) + 9);
    for (let i = 0; i < slice.length; i++) {
-   splitArray.push(slice[i]);
+      splitArray.push(slice[i]);
    }
 }
 
@@ -69,8 +59,8 @@ function showPage (arr) {
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+Creates the `addPagination` function to create page navigation links depending on the number
+of pages and highlights the current `active` page
 */
 
 let pageLinks = document.querySelector('.link-list');
@@ -79,9 +69,9 @@ function getNumberOfPages (arr) {
    numberOfPages = Math.ceil(arr.length/9);
 }
 
-function createPaginationLinks(num1, arr1) {
+function addPagination(num, arr) {
    pageLinks.innerHTML = '';
-   for (let i = 0; i < num1; i++) {
+   for (let i = 0; i < num; i++) {
       let li = document.createElement('li');
       let button = document.createElement('button');
       button.innerHTML = i+1;
@@ -91,35 +81,36 @@ function createPaginationLinks(num1, arr1) {
          currentPage = +e.target.id;
          allButtons = pageLinks.getElementsByTagName('BUTTON');
          for ( i = 0; i < allButtons.length; i++) {
-            allButtons[i].classList.remove('active');
+            allButtons[i].className = 'INactive';
          }
-         sliceArray(arr1);
+         sliceArray(arr);
          showPage(splitArray);
-         if(buttonID == currentPage) {
+         if(buttonID === currentPage) {
             button.className = "active";
-      }
-   })
+         }
+      })
       li.appendChild(button);
       pageLinks.appendChild(li);
-      if(buttonID == currentPage) {
+      if(buttonID === currentPage) {
          button.className = "active";
+      }
    }
-   }
-   
 }
 
-// Search Bar
+// Creates and inserts the Search Bar
 
 const header = document.querySelector('.header');
 const searchBox = document.createElement('label');
-header.appendChild(searchBox);
 searchBox.for = 'search';
 searchBox.className = "student-search";
 searchBox.innerHTML = `<span>Search by name</span>
    <input id="search" placeholder="Search by name...">
-   <button type="button" id="search button"><img src="img/icn-search.svg" alt="Search icon"></button>`
+   <button type="button" id="search button"><img src="img/icn-search.svg" alt="Search icon"></button>`;
+header.appendChild(searchBox);
 
-// Search Function
+/* 
+Creates the search Function, which creates a new array called searchArray containing all matching records and displays them, or `no results` if none
+*/
 
 const search = document.getElementById('search');
 const submit = document.getElementById('search button');
@@ -129,40 +120,38 @@ function searchFunction (item, names) {
    currentPage = 1;
    searchArray = [];
    for (let i = 0; i<names.length; i++) {
-   if(item.value.length !== 0 && names[i].firstElementChild.firstElementChild.nextElementSibling.textContent.toLowerCase().includes(item.value.toLowerCase())){
-      searchArray.push(names[i]);
+      if (item.value.length !== 0 && names[i].firstElementChild.firstElementChild.nextElementSibling.textContent.toLowerCase().includes(item.value.toLowerCase())) {
+         searchArray.push(names[i]);
+      }
+   if (item.value.length === 0) {
+      searchArray = studentArray;
    }
    }
 }
-function noSearchResults () {
-   if (searchArray.length === 0 && splitArray.length === 0) {
-      ul.insertAdjacentHTML("afterbegin", `<span class = "no results">No Results found.</span>`)
+
+function noSearchResults (arr) {
+   if (arr.length === 0) {
+      ul.insertAdjacentHTML("afterbegin", `<span class = "no-results">No Results found.</span>`)
    }
 }
 
-submit.addEventListener('click', (e) => {
-   e.preventDefault();
-   searchFunction(search, studentArray);
-   sliceArray(searchArray);
-   showPage(splitArray);
-   getNumberOfPages(searchArray);
-   createPaginationLinks(numberOfPages, searchArray, splitArray);
-   noSearchResults();
-});
-
-search.addEventListener('keyup', () => {
-   searchFunction(search, studentArray);
-   sliceArray(searchArray);
-   showPage(splitArray);
-   getNumberOfPages(searchArray);
-   createPaginationLinks(numberOfPages, searchArray, splitArray);
-   noSearchResults();
-})
+function addListeners (item1, item2) {
+   item1.addEventListener(item2, (e) => {
+      searchFunction(search, studentArray);
+      sliceArray(searchArray);
+      showPage(splitArray);
+      getNumberOfPages(searchArray);
+      addPagination(numberOfPages, searchArray, splitArray);
+      noSearchResults(searchArray);
+   })
+}
 
 // Call functions
 
-CreateLI(data);
+createLI(data);
 getNumberOfPages(studentArray);
 sliceArray(studentArray, currentPage);
 showPage(splitArray, currentPage);
-createPaginationLinks(numberOfPages, studentArray, splitArray);
+addPagination(numberOfPages, studentArray, splitArray);
+addListeners(submit, 'click');
+addListeners(search, 'keyup');
